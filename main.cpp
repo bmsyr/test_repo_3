2c42f712a2c55c316e6cce9fee988d84ff69e66a
BogdanSRTCH <bogdansrtch@oxygen>
2023-11-24 08:26:40 +0300

    Update main.cpp
    Solve problem in the simplest way

--- main.cpp
+++ main.cpp
@@ -3,5 +3,28 @@
 #include <string>
 
 int main() {
-    
+    std::deque<std::string> works;
+
+    int n = 0;
+    std::cin >> n;
+//Наполнение дека работ из файла
+    for (int i = 0; i < n; ++i) {
+        std::string name, position;
+        std::cin >> name >> position;
+
+        if (position == "top") {
+            works.push_front(name);
+        } else {
+            works.push_back(name);
+        }
+    }
+
+    int k = 0;
+    std::cin >> k;
+//Вывод фамилий студентов, чьи работы будут проверены
+    for (int i = 0; i < k; ++i) {
+        int x = 0;
+        std::cin >> x;
+        std::cout << works[x - 1] << "\n";
+    }
 }
